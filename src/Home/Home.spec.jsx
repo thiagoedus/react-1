9431b8a599449f97a3627a1501b93e7a8e209e38
Thiago Santos <playsd395@gmail.com>
2024-08-05 15:45:56 -0300

    Testado Button renderizado pela Home

--- src/Home/Home.spec.jsx
+++ src/Home/Home.spec.jsx
@@ -1,57 +1,68 @@
-import { rest } from 'msw';
-const { setupServer } = require('msw/node');
+// import { rest } from 'msw';
+// const { setupServer } = require('msw/node');
 
 import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
 import { Home } from '.';
 
-const handlers = [
-  rest.get('*jsonplaceholder.typicode.com*', async (req, res, ctx) => {
-    return res(
-      ctx.json([
-        {
-          userId: 1,
-          id: 1,
-          title: 'title1',
-          body: 'body1',
-          url: 'img1.jpg',
-        },
-        {
-          userId: 2,
-          id: 2,
-          title: 'title2',
-          body: 'body2',
-          url: 'img1.jpg',
-        },
-        {
-          userId: 3,
-          id: 3,
-          title: 'title3',
-          body: 'body3',
-          url: 'img3.jpg',
-        },
-      ]),
-    );
-  }),
-];
+// const handlers = [
+//   rest.get('*jsonplaceholder.typicode.com*', async (req, res, ctx) => {
+//     return res(
+//       ctx.json([
+//         {
+//           userId: 1,
+//           id: 1,
+//           title: 'title1',
+//           body: 'body1',
+//           url: 'img1.jpg',
+//         },
+//         {
+//           userId: 2,
+//           id: 2,
+//           title: 'title2',
+//           body: 'body2',
+//           url: 'img1.jpg',
+//         },
+//         {
+//           userId: 3,
+//           id: 3,
+//           title: 'title3',
+//           body: 'body3',
+//           url: 'img3.jpg',
+//         },
+//       ]),
+//     );
+//   }),
+// ];
 
-const server = setupServer(...handlers);
+// const server = setupServer(...handlers);
 
 describe('<Home />', () => {
-  beforeAll(() => {
-    server.listen();
-  });
+  // beforeAll(() => {
+  //   server.listen();
+  // });
 
-  afterEach(() => server.resetHandlers());
+  // afterEach(() => server.resetHandlers());
 
-  afterAll(() => {
-    server.close();
-  });
+  // afterAll(() => {
+  //   server.close();
+  // });
 
   it('should render search, posts and load more', async () => {
     render(<Home />);
-    const noMorePosts = screen.getByText('Não existem posts =(');
+    const noMorePosts = screen.getByText('Não existem posts');
+
+    // expect.assertions(2);
 
     await waitForElementToBeRemoved(noMorePosts);
-    screen.debug();
+
+    const search = screen.getByPlaceholderText(/type your search/i);
+    expect(search).toBeInTheDocument();
+
+    const images = screen.getAllByRole('img');
+    expect(images).toHaveLength(10);
+
+    const button = screen.getByRole('button', { name: /Load More/i });
+
+    expect(button).toBeInTheDocument();
   });
 });
